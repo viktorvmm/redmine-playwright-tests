548dbd90d9719416b2c3dbd2891fe2baba02d336
Viktor Mironiuk <qa.viktormironiuk@gmail.com>
2025-06-17 12:49:27 +0300

    refactor: remove API status code checks

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,11 +1,9 @@
 const { expect } = require('@playwright/test');
 const { test } = require('../fixtures/pages');
-const HttpStatusChecker = require('../utils/HttpStatusChecker');
 
 test.describe('Redmine Homepage Tests', () => {
   test('Verify Homepage Loads Successfully', async ({ homePage }) => {
-    const response = await homePage.goto();
-    await HttpStatusChecker.verifySuccess(response);
+    await homePage.goto();
     await homePage.verifyPageTitle();
     await homePage.verifyHomepageLoaded();
   });
